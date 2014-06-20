59ce8bb890f8c2c997b8192fbac5b06fa279e027
BHASKAR KALIA <bhaskarkalia11github@gmail.com>
2014-06-20 10:51:42 +0530

    adding project files

---
+++ Easy_Calculator/src/bhaskar/easycalculator/about.java
@@ -0,0 +1,17 @@
+package bhaskar.easycalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class about extends Activity  {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+		
+			
+		
+	}
+
+}
+
